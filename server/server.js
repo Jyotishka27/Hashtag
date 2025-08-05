f72620fe39bfcae73393f51fece94db234caff63
Jyotishka Mukhopadhyay <76933417+Jyotishka27@users.noreply.github.com>
2025-08-05 23:16:47 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server running on port 5000");
+    });
+  })
+  .catch(err => console.error("Mongo error:", err));
